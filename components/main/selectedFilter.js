2a834193535e8f472b999dffa6b1cbda7b370c5d
Linda Balan <lindabalan.j@gmail.com>
2023-11-27 23:29:11 +0530

    Added delete and clear all logic; yet to add logic for unselecting the checkbox in sidenav bar.

--- components/main/selectedFilter.js
+++ components/main/selectedFilter.js
@@ -1,19 +1,39 @@
 import * as React from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { activeFilterData } from '../../redux/actions/selectorFilterAction';
 
 export default function SelectedFilter() {
     const { activeFilterList } = useSelector(state => state.selector);
-    const handleDelete = () => {
-        alert('You clicked the delete icon.');
+    const dispatch = useDispatch();
+    const handleDelete = (option) => {
+        const filterIndex = activeFilterList.indexOf(option)
+        const newFilter = [...activeFilterList]
+        newFilter.splice(filterIndex, 1)
+        dispatch(activeFilterData(newFilter))
+    };
+
+    const handleClearAll = () => {
+        dispatch(activeFilterData([]))
     };
 
     return (
-        <Stack direction="row" spacing={1}>
-            {activeFilterList.map((option, index) => (
-                <Chip label={option} key={option + index} sx={{ textTransform: 'capitalize' }} variant="outlined" onDelete={handleDelete} />
-            ))}
-        </Stack>
+        <div style={{ marginBottom: '10px' }}>
+            {activeFilterList.length >= 1 && <div style={{ marginBottom: '10px' }}> {activeFilterList.length} item(s) Selected. <span className='linklike' onClick={handleClearAll} >Clear All</span></div>}
+            <Stack direction="row" spacing={1}>
+                {activeFilterList.map((option, index) => (
+                    <Chip label={option} key={option + index} sx={{ textTransform: 'capitalize' }} variant="outlined" onDelete={(option) => handleDelete(option)} />
+                ))}
+            </Stack>
+            <style jsx>
+                {`.linklike{
+                        text-decoration: underline;
+                        color: #0d6efd;
+                        cursor: pointer;
+                        margin-left: 5px;
+                }`}
+            </style>
+        </div>
     );
 }
