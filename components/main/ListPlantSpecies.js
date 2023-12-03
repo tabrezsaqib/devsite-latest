82d82bc09e273ad42d953068cc42cebed280c77b
Linda Balan <lindabalan.j@gmail.com>
2023-12-03 17:55:38 +0530

    Sorted Plant tiles by latin name

--- components/main/ListPlantSpecies.js
+++ components/main/ListPlantSpecies.js
@@ -8,6 +8,7 @@ import styles from "../../styles/Global.module.scss"
 import loaderStyles from "../../styles/Loader.module.scss"
 import LoaderThumbnail from "./LoaderThumbnail"
 import SelectedFilter from "./selectedFilter"
+import { useEffect, useState } from "react"
 
 const ListPlantSpecies = ({
   plants_list,
@@ -15,54 +16,63 @@ const ListPlantSpecies = ({
   isLoading,
   filteredList,
 }) => {
-  // let filteredList
   const router = useRouter()
   const loaderDataCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  const [filteredListSorted, setFilteredListSorted] = useState([])
+  useEffect(() => {
+    const sorted = [...filteredList].sort((a, b) => {
+      if (a.title.rendered && b.title.rendered)
+      return a.title.rendered.localeCompare(b.title.rendered)
+      else
+      return a.title.localeCompare(b.title)
+    });
+    setFilteredListSorted(sorted)
+  }, [filteredList])
 
   return (
     <div >
       <SelectedFilter />
       <div className="d-flex flex-wrap">
-      {filteredList.length > 0 ? (
-        filteredList.map((data, index) => {
-          return (
-            <div
-              key={data.id}
-              className={[
-                "box-container",
-                styles.box_container_media_query,
-              ].join(" ")}>
-              <Link legacyBehavior
-                href={{
-                  pathname: `/plants/${data.slug}`,
-                  query: { type: data.acf.plant_type },
-                }}>
-                <a className="thumbnails">
-                  <PlantSpecies plant={data} />
-                </a>
-              </Link>
+        {filteredListSorted.length > 0 ? (
+          filteredListSorted.map((data, index) => {
+            return (
+              <div
+                key={data.id}
+                className={[
+                  "box-container",
+                  styles.box_container_media_query,
+                ].join(" ")}>
+                <Link legacyBehavior
+                  href={{
+                    pathname: `/plants/${data.slug}`,
+                    query: { type: data.acf.plant_type },
+                  }}>
+                  <a className="thumbnails">
+                    <PlantSpecies plant={data} />
+                  </a>
+                </Link>
+              </div>
+            )
+          })
+        ) : isLoading == true ? (
+          <div className="d-flex flex-wrap">
+            {loaderDataCount.map((data, index) => (
+              <div key={index} className={[loaderStyles.loader_box].join(" ")}>
+                <LoaderThumbnail />
+              </div>
+            ))}
+          </div>
+        ) : (
+          filteredListSorted.length == 0 &&
+          activeFilterList.length > 0 && (
+            <div className="info-section d-flex align-items-center justify-content-center">
+              <div className="d-flex flex-column text-center">
+                <img src="../../images/no_result_found.png" alt="" />
+                <h3>Oops! No data found!</h3>
+              </div>
             </div>
           )
-        })
-      ) : isLoading == true ? (
-        <div className="d-flex flex-wrap">
-          {loaderDataCount.map((data, index) => (
-            <div key={index} className={[loaderStyles.loader_box].join(" ")}>
-              <LoaderThumbnail />
-            </div>
-          ))}
-        </div>
-      ) : (
-        filteredList.length == 0 &&
-        activeFilterList.length > 0 && (
-          <div className="info-section d-flex align-items-center justify-content-center">
-            <div className="d-flex flex-column text-center">
-              <img src="../../images/no_result_found.png" alt="" />
-              <h3>Oops! No data found!</h3>
-            </div>
-          </div>
-        )
-      )}</div>
+        )}</div>
       <style jsx>{`
         .box-container {
           background-color: #ffffff;
