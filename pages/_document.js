63d57527cfad72b4c488e50afa9c086449d13a73
Linda Balan <lindabalan.j@gmail.com>
2024-04-09 22:37:09 +0530

    font fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,12 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.css"
-                    type="text/css"
-                    media="screen"
-                />
+
+               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
                 <link
                     href="https://cdn.lineicons.com/3.0/lineicons.css"
                     rel="stylesheet"
                 />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
-                    rel="stylesheet" />
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
             </Head>
             <body>
