477dfa451c0557758860eda9d32136726efad827
Linda Balan <lindabalan.j@gmail.com>
2024-07-17 16:18:43 +0530

    Sort by english name

--- components/main/plantListModal.js
+++ components/main/plantListModal.js
@@ -2,7 +2,6 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable camelcase */
-
 import React, { useEffect, useState } from "react";
 import ReactHtmlParser from "react-html-parser"
 
@@ -18,9 +17,10 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import SortIcon from '@mui/icons-material/Sort';
 
 function PlantListModal({ openModal, handleModal }) {
-    const [isLoading, setLoading] = useState(false)
-    const { filteredPlantList } = useSelector(state => state.post)
+    const [isLoading, setLoading] = useState(false);
+    const { filteredPlantList } = useSelector(state => state.post);
     const [alignment, setAlignment] = React.useState('latin');
+    const [plantList, setPlantList] = React.useState([]);
 
     const handleChange = (event, newAlignment) => {
         if (newAlignment !== null) {
@@ -28,6 +28,21 @@ function PlantListModal({ openModal, handleModal }) {
         }
     };
 
+    useEffect(() => {
+        if (alignment === 'latin')
+            setPlantList(filteredPlantList)
+        else {
+            let arr = [...filteredPlantList]
+            let a = arr.sort((a, b) => {
+                const nameA = a.acf.common_name.trim().toUpperCase(); // ignore upper and lowercase
+                const nameB = b.acf.common_name.trim().toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) return -1;
+                if (nameA > nameB) return 1;
+                return 0;
+            });
+            setPlantList(a)
+        }
+    }, [alignment])
 
     return (
         <>
@@ -38,7 +53,6 @@ function PlantListModal({ openModal, handleModal }) {
                             Plant List
                         </div>
                         <div>
-
                             <span className="sortBy" > Sort By:</span>
                             <ToggleButtonGroup
                                 size="small"
@@ -65,33 +79,32 @@ function PlantListModal({ openModal, handleModal }) {
                         <div className={[styles.imgContainer, "d-flex", 'center-align'].join(" ")}>
                             <img className={styles.imgContent} src="../../images/loading.gif" alt="loader" />
                         </div>) :
-                        filteredPlantList.length > 0 &&
+                        plantList.length > 0 &&
                         <div style={{ margin: '10px' }}>
-
                             <div className="row " >
-                                {filteredPlantList.map((family, i) => (
+                                {plantList.map((family, i) => (
                                     <>  <div key={i} className="listOfPlants  col-sm-12 col-md-6  col-lg-6 ">
                                         <Link legacyBehavior
                                             href={{
                                                 pathname: `/plants/${family.slug}`,
                                                 query: { type: family.acf.plant_type },
                                             }}>
-                                            <a className="familyLink">  {ReactHtmlParser(family.acf.latin)}</a>
+                                            <a className="familyLink">  { alignment === 'latin'?  ReactHtmlParser(family.acf.latin): ReactHtmlParser(family.acf.common_name)}</a>
                                         </Link> /
                                         <Link legacyBehavior
                                             href={{
                                                 pathname: `/plants/${family.slug}`,
                                                 query: { type: family.acf.plant_type },
                                             }}>
-                                            <a className="familyLink">  {ReactHtmlParser(family.acf.common_name)}</a>
+                                            <a className="familyLink"> { alignment === 'latin'?  ReactHtmlParser(family.acf.common_name): ReactHtmlParser(family.acf.latin)} </a>
                                         </Link>
 
 
-                                    </div></>))
+                                    </div></>
+                                    ))
                                 }</div>
                         </div>}
                 </DialogContent>
-
                 <Divider></Divider>
                 <DialogActions>
                     <Button sx={{ color: '#0e9d47', displayPrint: 'none' }} onClick={handleModal}>Close</Button>
